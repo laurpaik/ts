88d4517bb147bc60c92a377f223904d43f42261e
laurpaik <laurpaik@gmail.com>
2017-03-20 10:44:57 -0400

    Adds some pettable animals
    - exports pets.ts and imports it into pettable.ts

---
+++ animals.ts
@@ -0,0 +1,19 @@
+const stray = {
+  numberOfPets: 0,
+  isAGoodBoy: true,
+  legs: 3
+};
+
+class Panda {
+  public numberOfPets: number = 1;
+  public isAGoodBoy: boolean = true;
+  public eatsBamboo = 'nomnomnom';
+}
+
+class Otter {
+  public numberOfPets: number = 1;
+  public isAGoodBoy: boolean = true;
+  public holdHands = 'ZZZ';
+}
+
+export { Panda, Otter, stray };
--- pettable.ts
+++ pettable.ts
@@ -1,3 +1,5 @@
+import { Panda, Otter, stray } from "./animals";
+
 interface Pettable {
 	numberOfPets: number;
 	isAGoodBoy: boolean;
@@ -7,3 +9,24 @@ function pet(animal: Pettable) {
 	animal.numberOfPets++;
 	animal.isAGoodBoy = true;
 }
+
+class Elephant {
+  public numberOfPets: number = 1;
+  public isAGoodBoy: boolean = true;
+}
+
+ class Aardvark {
+   public numberOfPets: number = 1;
+   public isAGoodBoy: boolean = true;
+   public bla = 'blablabla';
+ }
+// in order for the elephant to be pettable/ to run pet(elephant)
+// need both properties in the interface
+let e = new Elephant();
+let a = new Aardvark();
+pet(e);
+
+const o = new Otter();
+const p = new Panda();
+pet(o)
+pet(p)
